7975a5a713baf87eb0afb53fb344f944258ec441
Tomasz Pisarek <tomasz.pisarek@gmail.com>
2018-11-16 19:11:29 +0100

    change information page

--- src/components/Pages/Information/Information.js
+++ src/components/Pages/Information/Information.js
@@ -87,6 +87,24 @@ class Information extends Component {
               </div>
             </div>
           </div>
+          <div className={g.container}>
+            <h4 className="pageTitle">
+              Hotel
+            </h4>
+            <div className={[g.row, c.Box2].join(" ")}>
+              <div className={g.col}>
+                <p>Rezerwacja na hasło <span className={c.Bold}>Akademia Gwiazd</span> na adres mailowy:
+                <a href="mailto:reservation.andels-lodz@viennahouse.com"><span className={c.Red}>reservation.andels-lodz@viennahouse.com</span></a> lub telefonicznie <a
+                  href="tel:+48422791521"><span className={c.Red}>42&nbsp;279&nbsp;15&nbsp;21/22</span></a></p>
+
+                <p>Cena za pokój jednoosobowy ze śniadaniem 380 zł brutto z 8% VAT<br/>
+                Cena za pokój dwuosobowy ze śniadaniem 420 zł brutto z 8% VAT</p>
+
+                <p>Stawki obowiązują w dobie 30.11-01.12.2018.<br/>
+                Pokoje udostępniane zgodnie z dostępnością.</p>
+              </div>
+            </div>
+          </div>
         </div>
     );
   }
